007f0100b3e540621bc6c9d1694913c9511317ce
Braian <braian.escher@gmail.com>
2025-04-01 08:57:04 -0300

    ADD- inicial

---
+++ validador.js
@@ -0,0 +1,29 @@
+function validarFormulario() {
+    const nome = document.getElementById('nome').value.trim();
+    const cpf = document.getElementById('cpf').value.trim();
+    const idade = parseInt(document.getElementById('idade').value,10);
+    const experiencia = parseInt(document.getElementById('experiencia').value,10);
+
+    console.log(nome);
+    console.log(cpf);
+    console.log(idade);
+    console.log(experiencia);
+
+    if (!nome) {
+        alert('por favor, preencha o seu nome');
+        return;
+    }
+    if (!cpf || cpf.length !== 11 || isNaN(cpf)) {
+        alert('por favor, insira um CPF valido');
+        return
+    }
+    if (!idade || idade < 18 || isNaN(idade)) {
+        alert('você deve ser maior de idade para ser um maquinista');
+        return
+    }
+    if (isNaN (experiencia) || experiencia <= 0){
+        alert('você deve ter algum tempo de experiencia para trabalhar conosco');
+        return
+    }
+}
+validarFormulario()
