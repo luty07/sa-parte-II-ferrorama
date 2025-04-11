f8545f5daed68b2c9850cee5b27b1ef9d5a6d24f
perolavieiraa <perola_vieira@estudante.sesisenai.org.br>
2025-04-11 12:40:32 -0300

    add

--- pscript.js
+++ pscript.js
@@ -4,23 +4,23 @@ function redirectTo(page) {
 
 function login() {
     alert("Login realizado com sucesso!");
-    redirectTo("home.html");
+    redirectTo("pinicio.html");
 }
 
 function cadastrar() {
     alert("Conta criada com sucesso!");
-    redirectTo("tela7.html");
+    redirectTo("ptela7.html");
 }
 
 function recuperarSenha() {
     alert("Um email de recuperação foi enviado!");
-    redirectTo("tela4.html");
+    redirectTo("ptela4.html");
 }
 
 document.addEventListener("DOMContentLoaded", function () {
     if (document.querySelector(".progress")) {
         setTimeout(() => {
-            window.location.href = "home.html"; 
+            window.location.href = "pinicio.html"; 
         }, 3000);
     }
 });
