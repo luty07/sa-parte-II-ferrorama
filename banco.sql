5bb5ce6e5adb1feed03069e7db06c161af06050e
Heloysa Sales <heloysa_sales@estudante.sesisenai.org.br>
2025-09-11 12:41:53 -0300

    add - fiz o banco de dados.

---
+++ banco.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE login_system;
+
+USE login_system;
+
+CREATE TABLE usuarios (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
