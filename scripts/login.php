9536f78b7e948d7a6f1b04e5fec551434ecfbfc4
Heloysa Sales <heloysa_sales@estudante.sesisenai.org.br>
2025-09-11 12:05:42 -0300

    add - login

--- scripts/login.php
+++ scripts/login.php
@@ -1,7 +1,13 @@
 <?php
-$host="localhost"
-$user="root"
-$password="root"
-$db ="Nome do banco de dados"
+$host="localhost";
+$user="root";
+$password="root";
+$db ="Nome do banco de dados";
 
-$mysqli=new mysqli($host, $user, $password, $db)
+$mysqli=new mysqli($host, $user, $password, $db);
+
+if($mysqli>conecct_errno) {
+    die("Falha na conexão:".$mysqli->connect_error);
+}
+
+$errorEmail
