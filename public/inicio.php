baf54a744806e3e9b45c953d28f165015eac0049
Heloysa Sales <heloysa_sales@estudante.sesisenai.org.br>
2025-09-16 08:39:15 -0300

    add

--- public/inicio.php
+++ public/inicio.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-// Se o usuário não estiver logado, redireciona para a página de login
+
 if (!isset($_SESSION['usuario_id'])) {
     header("Location: login.php");
     exit;
