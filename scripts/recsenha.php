eaf8062f11ca73046a84b71809bd71a122d35efc
HelenaBohn <helena_b_martins@estudante.sesisenai.org.br>
2025-09-25 10:55:20 -0300

    add recsenha.php

--- scripts/recsenha.php
+++ scripts/recsenha.php
@@ -1,6 +1,7 @@
+<?php
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,25 +9,19 @@
     <link rel="stylesheet" href="../styles/pstyle.css">
     <script src="../scripts/senharec.js"></script>
 </head>
-
 <body>
     <div class="container">
         <h1>Recuperar Senha</h1>
 
-        <form id="formulario">
-
+        <form id="formulario" method="post" action="codemail.php">
             <label for="email">Email:</label>
             <input type="email" id="email" name="email">
             <div class="error" id="erroEmail"></div>
 
-
-            <a href="codemail.html"><button>Enviar</button></a>
-
+            <button type="submit">Enviar</button>
         </form>
 
-        <a href="../index.html">Voltar</a>
+        <a href="../index.php">Voltar</a>
     </div>
-
 </body>
-
 </html>
