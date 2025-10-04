5d0c83dbc59d79a7bf34df65bf9032d44c89d925
Braian Lucas Escher <braian.escher@gmail.com>
2025-10-04 15:44:36 -0300

    UPDATE- pagina usuario criada

--- index.php
+++ index.php
@@ -99,6 +99,8 @@ $mysqli->close();
             <a href="scripts/recsenha.php">Esqueci minha senha</a>
             <br>
             <a href="scripts/criarconta.php">Criar Conta</a>
+            <br>
+            <a href="scripts/userconfig.php">userconfig</a>
         </div>
     </div>
 </body>
