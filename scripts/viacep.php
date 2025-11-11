31b73ee5e97c48e43d4d188211aac9b20eecec13
perolavieiraa <perola_vieira@estudante.sesisenai.org.br>
2025-11-11 09:38:21 -0300

    ADD - botao de voltar

--- scripts/viacep.php
+++ scripts/viacep.php
@@ -32,9 +32,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 <head>
     <meta charset="UTF-8">
     <title>RAILTRACK</title>
-    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/pstyle.css">
 </head>
 <body>
+    <div id="cabecalho">
+  	<a href="inicio.php"><button>↩</button></a>
+  </div>
     <h2>Consulta de CEP - RAILTRACK</h2>
 
     <form method="POST" action="">
